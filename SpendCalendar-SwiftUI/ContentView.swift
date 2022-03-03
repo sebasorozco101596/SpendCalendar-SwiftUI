19f3eb877ba154f75512c80d985deded402d96d0
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-02 23:01:07 -0500

    Initial Commit

---
+++ SpendCalendar-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpendCalendar-SwiftUI
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpendCalendar-SwiftUI/SpendCalendar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendCalendar_SwiftUIApp.swift
+//  SpendCalendar-SwiftUI
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SpendCalendar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
